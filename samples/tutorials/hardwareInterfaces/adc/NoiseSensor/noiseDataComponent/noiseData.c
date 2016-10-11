8c5a6d363320cfafd8e9a2943c61e7ee7abbb54f
ashish syal <asyal@sierrawireless.com>
2016-10-11 20:37:42 +0200

    GPIO and ADC mangOH Green tutorials

---
+++ samples/tutorials/hardwareInterfaces/adc/NoiseSensor/noiseDataComponent/noiseData.c
@@ -0,0 +1,58 @@
+//--------------------------------------------------------------------------------------------------
+/** 
+ * This app reads the current adc reading every 1 seconds 
+ */
+//--------------------------------------------------------------------------------------------------
+#include "legato.h"
+#include "interfaces.h"
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * The time between publishing ADC location readings
+ *
+ * @note Please change this timeout value as needed.
+ */
+//--------------------------------------------------------------------------------------------------
+#define ADC_SAMPLE_INTERVAL_IN_SECONDS (1)
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Timer handler that will publish the current ADC reading.
+ *
+ * @note
+ *      
+ */
+//--------------------------------------------------------------------------------------------------
+static void adcTimer
+(
+    le_timer_Ref_t adcTimerRef
+)
+{
+    int32_t value;
+
+    const le_result_t result = le_adc_ReadValue("EXT_ADC1",&value);
+
+    if (result == LE_OK)
+    {
+        LE_INFO("EXT_ADC1 value is: %d", value);
+    }
+    else
+    {
+        LE_INFO("Couldn't get ADC value");
+    }
+}
+
+COMPONENT_INIT
+{
+   LE_INFO("---------------------- ADC Reading started");
+
+    le_clk_Time_t  timerInterval = {.sec = ADC_SAMPLE_INTERVAL_IN_SECONDS, .usec = 0};
+    le_timer_Ref_t adcTimerRef;
+    adcTimerRef = le_timer_Create("ADC Timer");
+    le_timer_SetInterval(adcTimerRef, timerInterval);
+    le_timer_SetRepeat(adcTimerRef, 0);
+
+    le_timer_SetHandler(adcTimerRef, adcTimer);
+
+    le_timer_Start(adcTimerRef);
+}
---
+++ samples/tutorials/hardwareInterfaces/gpio/TimerLed/timerLedComponent/timerLed.c
@@ -0,0 +1,66 @@
+//--------------------------------------------------------------------------------------------------
+/*
+ * This sample app is a blinking LED at 1 second interval
+ */
+//--------------------------------------------------------------------------------------------------
+
+#include "legato.h"
+#include "interfaces.h"
+
+#define LED_SAMPLE_INTERVAL_IN_SECONDS (1)
+
+static int8_t state = 0;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Sets default configuration LED D750 as off
+ */
+//--------------------------------------------------------------------------------------------------
+static void ConfigureLed
+(
+    void
+)
+{
+    // Configure initial value as LED off
+    LE_FATAL_IF(mangoh_ledGpio_SetPushPullOutput(MANGOH_LEDGPIO_ACTIVE_HIGH, true) != LE_OK,
+                "Couldn't configure LED PLAY as a push pull output");
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * LED D750 changes state as defined by LED_SAMPLE_INTERVAL_IN_SECONDS 
+ */
+//--------------------------------------------------------------------------------------------------
+static void ledTimer
+(
+    le_timer_Ref_t ledTimerRef
+)
+{
+    mangoh_ledGpio_SetEdgeSense(MANGOH_LEDGPIO_EDGE_BOTH);
+    if (state == true)
+    {
+        mangoh_ledGpio_Activate();
+        state = false;
+    }
+    else
+    {
+        mangoh_ledGpio_Deactivate();
+        state = true;
+    }
+}
+
+COMPONENT_INIT
+{
+    LE_INFO("===============blinking LED application has started");
+
+    ConfigureLed();
+
+    le_clk_Time_t timerInterval = {.sec = LED_SAMPLE_INTERVAL_IN_SECONDS, .usec = 0};
+    le_timer_Ref_t ledTimerRef;
+    ledTimerRef = le_timer_Create("LED Timer");
+    le_timer_SetInterval(ledTimerRef, timerInterval);
+    le_timer_SetRepeat(ledTimerRef, 0);
+
+    le_timer_SetHandler(ledTimerRef,ledTimer);
+    le_timer_Start(ledTimerRef);
+}
