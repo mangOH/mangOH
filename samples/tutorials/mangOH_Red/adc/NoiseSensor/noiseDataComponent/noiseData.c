890df65751e629317fdae6f306c200ef3616a00e
Mirac Chen <mchen@sierrawireless.com>
2017-01-24 10:03:59 -0800

    Port noise sensor tutorial to Red

---
+++ samples/tutorials/mangOH_Red/adc/NoiseSensor/noiseDataComponent/noiseData.c
@@ -0,0 +1,63 @@
+/**
+ * @file
+ *
+ * This app reads the current adc reading every 1 seconds
+ *
+ * <HR>
+ *
+ * Copyright (C) Sierra Wireless, Inc. Use of this work is subject to license.
+ */
+
+#include "legato.h"
+#include "interfaces.h"
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * The time between publishing ADC location readings
+ *
+ * @note Please change this timeout value as needed.
+ */
+//--------------------------------------------------------------------------------------------------
+#define ADC_SAMPLE_INTERVAL_IN_MILLISECONDS (1000)
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Timer handler  will publish the current ADC reading.
+ */
+//--------------------------------------------------------------------------------------------------
+static void adcTimer
+(
+    le_timer_Ref_t adcTimerRef
+)
+{
+    int32_t value;
+
+    const le_result_t result = le_adc_ReadValue("EXT_ADC0", &value);
+
+    if (result == LE_OK)
+    {
+        LE_INFO("EXT_ADC0 value is: %d", value);
+    }
+    else
+    {
+        LE_INFO("Couldn't get ADC value");
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Main program starts here
+ */
+//--------------------------------------------------------------------------------------------------
+COMPONENT_INIT
+{
+    LE_INFO("---------------------- ADC Reading started");
+
+    le_timer_Ref_t adcTimerRef = le_timer_Create("ADC Timer");
+    le_timer_SetMsInterval(adcTimerRef, ADC_SAMPLE_INTERVAL_IN_MILLISECONDS);
+    le_timer_SetRepeat(adcTimerRef, 0);
+
+    le_timer_SetHandler(adcTimerRef, adcTimer);
+
+    le_timer_Start(adcTimerRef);
+}
