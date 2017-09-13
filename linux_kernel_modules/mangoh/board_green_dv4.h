b35a81c5ff9910c3d9ab1ef13dbcaf831efa9ed0
David Frey <dfrey@sierrawireless.com>
2017-09-13 16:01:34 -0700

    Rework mangoh module into two modules
    There is now a mangoh_red and a mangoh_green module.  The universal
    mangoh module did very little in terms of code sharing so it made
    sense to split it into two simpler modules.

--- linux_kernel_modules/mangoh/board_green_dv4.h
+++
@@ -1,9 +0,0 @@
-#ifndef BOARD_GREEN_DV4_H
-#define BOARD_GREEN_DV4_H
-
-#include "mangoh.h"
-
-extern const struct mangoh_descriptor green_dv4_descriptor;
-int green_dv4_create_device(struct platform_device** d);
-
-#endif // BOARD_GREEN_DV4_H
