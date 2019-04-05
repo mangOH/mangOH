2f05e797a75ebb20807f33c8085c9d16f9857cec
David Frey <dfrey@sierrawireless.com>
2019-04-04 22:06:37 -0700

    simplify implementation of YellowOnBoardActuators
    This new version uses substantially less code to do nearly the same
    thing. Data hub inputs are no longer created because they were not used
    and I believe that clients should be observing the things that control
    the LEDs rather than the LEDs themselves.

---
+++ apps/YellowOnBoardActuators/components/outputActuator/outputActuator.h
@@ -0,0 +1,6 @@
+#ifndef _OUTPUT_ACTUATOR_H_
+#define _OUTPUT_ACTUATOR_H_
+
+LE_SHARED le_result_t RegisterOutputActuator(const char *dhubBasePath, const char *sysfsPath);
+
+#endif // _OUTPUT_ACTUATOR_H_
