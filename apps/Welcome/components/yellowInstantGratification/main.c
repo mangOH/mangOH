d34cb2c6161ce55c592e68ebb3dbea79ccf2c678
Jen Chitty <jchitty@sierrawireless.com>
2019-07-22 11:16:34 -0700

    Add config to disable instant gratification
    Make it possible to persistently disable the instant gratification
    features through a config tree entry.

--- apps/Welcome/components/yellowInstantGratification/main.c
+++ apps/Welcome/components/yellowInstantGratification/main.c
@@ -1,7 +1,32 @@
+//--------------------------------------------------------------------------------------------------
+/**
+ * Implementation of the "instant gratification" features for the out-of-box experience.
+ * These are things like blinking LEDs and responding to button presses.
+ *
+ * Copyright (C) Sierra Wireless Inc.
+ */
+//--------------------------------------------------------------------------------------------------
+
 #include "legato.h"
 #include "interfaces.h"
 
-COMPONENT_INIT
+/// Path to the instant gratification enable/disable configuration in the Config Tree.
+/// @note There's a bug in le_cfg_AddChangeHandler() that makes it not work unless we specify
+///       the tree name explicitly at the beginning of the configTree path.
+///       (This was discovered in Legato version 19.04).
+static const char ConfigPath[] = "helloYellow:/enableInstantGrat";
+
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Enable the instant gratification features, like the vegas mode and buzzer on button press.
+ */
+//--------------------------------------------------------------------------------------------------
+static void EnableInstantGratification
+(
+    void
+)
+//--------------------------------------------------------------------------------------------------
 {
     // Route button press events to the "vegas mode" trigger and the buzzer enable.
     dhubAdmin_SetSource("/app/vegasMode/triggered/trigger", "/app/button/value");
@@ -18,4 +43,71 @@ COMPONENT_INIT
 
     // Enable continuous (slow) Vegas Mode.
     dhubAdmin_SetBooleanOverride("/app/vegasMode/continuous/enable", true);
+}
+
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Disable the instant gratification features, like the vegas mode and buzzer on button press.
+ */
+//--------------------------------------------------------------------------------------------------
+static void DisableInstantGratification
+(
+    void
+)
+//--------------------------------------------------------------------------------------------------
+{
+    // Remove routings from button to "vegas mode" trigger and buzzer enable.
+    dhubAdmin_RemoveSource("/app/vegasMode/triggered/trigger");
+    dhubAdmin_RemoveSource("/app/buzzer/enable");
+
+    // Remove buzzer duty cycle override.
+    dhubAdmin_RemoveOverride("/app/buzzer/percent");
+
+    // Disable continuous (slow) Vegas Mode.
+    dhubAdmin_RemoveOverride("/app/vegasMode/continuous/enable");
+    dhubAdmin_PushBoolean("/app/vegasMode/continuous/enable", 0, false);
+
+    // Remove routings from vegasMode app to the LED control outputs.
+    dhubAdmin_RemoveSource("/app/leds/mono/enable");
+    dhubAdmin_RemoveSource("/app/leds/tri/red/enable");
+    dhubAdmin_RemoveSource("/app/leds/tri/green/enable");
+    dhubAdmin_RemoveSource("/app/leds/tri/blue/enable");
+}
+
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Function called-back by the Config Tree when the instant gratification enable/disable
+ * configuration changes.
+ */
+//--------------------------------------------------------------------------------------------------
+static void HandleConfigChange
+(
+    void* contextPtr
+)
+//--------------------------------------------------------------------------------------------------
+{
+    if (le_cfg_QuickGetBool(ConfigPath, true))
+    {
+        EnableInstantGratification();
+    }
+    else
+    {
+        DisableInstantGratification();
+    }
+}
+
+
+COMPONENT_INIT
+{
+    // Register for notification of change of the instant gratification enable/disable
+    // configuration in the Config Tree.
+    LE_ASSERT(le_cfg_AddChangeHandler(ConfigPath, HandleConfigChange, NULL));
+
+    // If the instant gratification features are enabled, start them up.
+    if (le_cfg_QuickGetBool(ConfigPath, true))
+    {
+        EnableInstantGratification();
+    }
 }
