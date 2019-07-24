2ba2da9ba40cf01007aff8b1b3b6d0fb2a1efa4d
Jen Chitty <jchitty@sierrawireless.com>
2019-07-23 17:31:30 -0700

    Update environment app data hub resources
    - Remove old JSON configuration setting.
    - Add enable and lowPower Boolean settings.
    - Make lowPower optional.
    - lowPower defaults to false.
    - Don't enable sensor until enable is true and at least
      one ambientAirTemp sample is received.
    - Remove dependency on libjansson.

--- apps/Bme680EnvironmentalSensor/bsec/component.c
+++ apps/Bme680EnvironmentalSensor/bsec/component.c
@@ -1,9 +1,22 @@
+//--------------------------------------------------------------------------------------------------
+/**
+ * Implementation of the Data Hub interface to the environment sensor.
+ *
+ * Copyright (C) Sierra Wireless Inc.
+ */
+//--------------------------------------------------------------------------------------------------
+
 #include "legato.h"
 #include "interfaces.h"
-#include <jansson.h>
 
 /// Input resource path
 #define RES_PATH_VALUE        "value"
+
+/// Data hub resource path for enable output.
+#define RES_PATH_ENABLE       "enable"
+
+/// Data hub resource path for low-power mode control output.
+#define RES_PATH_LOW_POWER    "lowPower"
 
 /// Ambient temperature output resource path.
 /// Providing an accurate measurement of ambient air temperature from another source improves the
@@ -15,156 +28,196 @@
 #define RES_PATH_CONFIG       "config"
 
 /// Sample JSON value to report to the Data Hub for auto-discovery by administrative tools.
-#define VALUE_EXAMPLE "{\"timestamp\":1,\"iaqValue\":1,\"iaqAccuracy\":1,\"co2EquivalentValue\":1,"\
-                      "\"co2EquivalentAccuracy\":1,\"breathVocValue\":1,\"breathVocAccuracy\":1,"\
-                      "\"pressure\":1,\"temperature\":1,\"humidity\":1 }"
+#define VALUE_EXAMPLE "{\"timestamp\":87455639412210,\"iaqValue\":25.0,\"iaqAccuracy\":0," \
+                      "\"co2EquivalentValue\":500.0,\"co2EquivalentAccuracy\":0,\"breathVocValue\""\
+                      ":0.49999994039535522,\"breathVocAccuracy\":0,\"pressure\":101621.0," \
+                      "\"temperature\":21.949438095092773,\"humidity\":50.673931121826172}"
 
-#ifdef BME680_DHUB_CONFIG
+/// true if at least one ambient temperature sample has been received.
+static bool AmbientTempReceived = false;
 
-#define CONFIG_EXAMPLE "{\"samplingRate\":\"SAMPLING_RATE_LP\",\"enableIaq\":true,"\
-                       "\"enableCo2Equivalent\":true,\"enableBreathVoc\":true,"\
-                       "\"enablePressure\":true,\"enableTemperature\":true,"\
-                       "\"enableHumidity\":true}"
+/// true if the sensor is enabled.
+static bool IsEnabled = false;
 
-static void BmeConfigPushHandler(double timestamp, const char *jsonStr, void *context)
+/// The sampling rate configuration.
+/// Defaults to "low power", which is actually the higher power consuming option.
+static mangOH_bme680_SamplingRate_t SamplingRate = MANGOH_BME680_SAMPLING_RATE_LP;
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Update the sensor configuration based on the configuration settings received from the
+ * Data Hub.
+ */
+//--------------------------------------------------------------------------------------------------
+static void UpdateSensor
+(
+    void
+)
 {
-    /*
-     * Expect jsonStr in the form:
-     * {
-     *   "samplingRate": "SAMPLING_RATE_LP",
-     *   "enableIaq": true,
-     *   "enableCo2Equivalent": true,
-     *   "enableBreathVoc": true,
-     *   "enablePressure": true,
-     *   "enableTemperature": true,
-     *   "enableHumidity": true
-     * }
-     */
+    mangOH_bme680_SamplingRate_t samplingRate = SamplingRate;
 
-    json_error_t loadError;
-    const size_t loadFlag = 0;
-    json_t *json = json_loads(jsonStr, loadFlag, &loadError);
-    if (json == NULL)
+    if ((!IsEnabled) || (!AmbientTempReceived))
     {
-        LE_ERROR("Received invalid JSON: \"%s\". Parsing gave error: %s", jsonStr, loadError.text);
-        return;
-
-    }
-    const char *samplingRate;
-    int enableIaq;
-    int enableCo2Equivalent;
-    int enableBreathVoc;
-    int enablePressure ;
-    int enableTemperature;
-    int enableHumidity;
-    int samplingRateEnum;
-
-    const int  unpackRes =  json_unpack(
-        json, "{s:s, s:b, s:b, s:b, s:b, s:b, s:b}", "samplingRate", &samplingRate, "enableIaq",
-        &enableIaq, "enableCo2Equivalent", &enableCo2Equivalent, "enableBreathVoc",
-        &enableBreathVoc, "enablePressure", &enablePressure, "enableTemperature",
-        &enableTemperature, "enableHumidity", &enableHumidity);
-
-    if (unpackRes)
-    {
-        LE_ERROR("Valid JSON has unexpected format: \"%s\"", jsonStr);
-        goto cleanup;
+        samplingRate = MANGOH_BME680_SAMPLING_RATE_DISABLED;
     }
 
-    if (strcmp(samplingRate, "SAMPLING_RATE_DISABLED") == 0)
-    {
-        samplingRateEnum = MANGOH_BME680_SAMPLING_RATE_DISABLED;
-    }
-    else if (strcmp(samplingRate, "SAMPLING_RATE_LP") == 0)
-    {
-        samplingRateEnum = MANGOH_BME680_SAMPLING_RATE_LP;
-    }
-    else if (strcmp(samplingRate, "SAMPLING_RATE_ULP") == 0)
-    {
-        samplingRateEnum = MANGOH_BME680_SAMPLING_RATE_ULP;
-    }
-    else
-    {
-        LE_ERROR("Bad sampling rate \"%s\"", samplingRate);
-        goto cleanup;
-    }
-
-    LE_ASSERT_OK(mangOH_bme680_Configure(
-       samplingRateEnum,
-       enableIaq,
-       enableCo2Equivalent,
-       enableBreathVoc,
-       enablePressure,
-       enableTemperature,
-       enableHumidity));
-
-    cleanup:
-        json_decref(json);
+    LE_ASSERT_OK(mangOH_bme680_Configure(samplingRate,
+                                         IsEnabled,    // IAQ
+                                         IsEnabled,    // CO2
+                                         IsEnabled,    // breath VOC
+                                         IsEnabled,    // pressure
+                                         IsEnabled,    // temperature
+                                         IsEnabled) ); // humidity
 }
 
-#endif // BME680_DHUB_CONFIG
 
-
+//--------------------------------------------------------------------------------------------------
+/**
+ * Function called back by the mangOH_bme680 API to pass a new sensor reading to us.
+ */
+//--------------------------------------------------------------------------------------------------
 static void SensorReadingHandler
 (
     const mangOH_bme680_Reading_t *reading,
     void* context
 )
 {
-    json_t *jsonBme = json_object();
+    char value[512];
 
-    json_object_set_new(jsonBme, "timestamp", json_real(reading->timestamp));
+    size_t len = snprintf(value, sizeof(value), "{\"timestamp\":%lld", reading->timestamp);
 
-    if (reading->iaq.valid)
+    if ((reading->iaq.valid) && (len < sizeof(value)))
     {
-        json_object_set_new(jsonBme, "iaqValue", json_real(reading->iaq.value));
-        json_object_set_new(jsonBme, "iaqAccuracy", json_integer(reading->iaq.accuracy));
+        len += snprintf(value + len,
+                        sizeof(value) - len,
+                        ",\"iaqValue\":%lf,\"iaqAccuracy\":%d",
+                        reading->iaq.value,
+                        reading->iaq.accuracy);
     }
 
-    if (reading->co2Equivalent.valid)
+    if ((reading->co2Equivalent.valid) && (len < sizeof(value)))
     {
-        json_object_set_new(jsonBme, "co2EquivalentValue", json_real(reading->co2Equivalent.value));
-        json_object_set_new(jsonBme, "co2EquivalentAccuracy",
-                            json_integer(reading->co2Equivalent.accuracy));
+        len += snprintf(value + len,
+                        sizeof(value) - len,
+                        ",\"co2EquivalentValue\":%lf,\"co2EquivalentAccuracy\":%d",
+                        reading->co2Equivalent.value,
+                        reading->co2Equivalent.accuracy);
     }
 
-    if (reading->breathVoc.valid)
+    if ((reading->breathVoc.valid) && (len < sizeof(value)))
     {
-        json_object_set_new(jsonBme, "breathVocValue", json_real(reading->breathVoc.value));
-        json_object_set_new(jsonBme, "breathVocAccuracy", json_integer(reading->breathVoc.accuracy));
+        len += snprintf(value + len,
+                        sizeof(value) - len,
+                        ",\"breathVocValue\":%lf,\"breathVocAccuracy\":%d",
+                        reading->breathVoc.value,
+                        reading->breathVoc.accuracy);
     }
 
-    if (reading->pressure.valid)
+    if ((reading->pressure.valid) && (len < sizeof(value)))
     {
-        json_object_set_new(jsonBme, "pressure", json_real(reading->pressure.value));
+        len += snprintf(value + len,
+                        sizeof(value) - len,
+                        ",\"pressure\":%lf",
+                        reading->pressure.value);
     }
 
-    if (reading->temperature.valid)
+    if ((reading->temperature.valid) && (len < sizeof(value)))
     {
-        json_object_set_new(jsonBme, "temperature", json_real( reading->temperature.value));
+        len += snprintf(value + len,
+                        sizeof(value) - len,
+                        ",\"temperature\":%lf",
+                        reading->temperature.value);
     }
 
-    if (reading->humidity.valid)
+    if ((reading->humidity.valid) && (len < sizeof(value)))
     {
-        json_object_set_new(jsonBme, "humidity", json_real(reading->humidity.value));
+        len += snprintf(value + len,
+                        sizeof(value) - len,
+                        ",\"humidity\":%lf",
+                        reading->humidity.value);
     }
 
-    char *value = json_dumps(jsonBme, JSON_COMPACT);
-    LE_ASSERT(value != NULL);
-    json_decref(jsonBme);
+    if (len < sizeof(value))
+    {
+        len += snprintf(value + len, sizeof(value) - len, "}");
+    }
+
+    LE_ASSERT(len < sizeof(value));
+
     io_PushJson(RES_PATH_VALUE, IO_NOW, value);
-    free(value);
 }
 
 
-static void AmbientTempPushHandler
+//--------------------------------------------------------------------------------------------------
+/**
+ * Function called by the Data Hub to deliver to us an update of the ambient temperature.
+ */
+//--------------------------------------------------------------------------------------------------
+static void HandleAmbientTempPush
 (
     double timestamp,
-    double value,
+    double value,   ///< degrees Celcius
     void* contextPtr
 )
 {
     mangOH_bme680_SetAmbientTemperature(value);
+
+    if (!AmbientTempReceived)
+    {
+        AmbientTempReceived = true;
+
+        UpdateSensor();
+    }
+}
+
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Function called by the Data Hub to deliver to us an update to our enable setting.
+ */
+//--------------------------------------------------------------------------------------------------
+static void HandleEnablePush
+(
+    double timestamp,
+    bool value,     ///< true = enable, false = disable
+    void* contextPtr
+)
+{
+    if (value != IsEnabled)
+    {
+        IsEnabled = value;
+        UpdateSensor();
+    }
+}
+
+
+//--------------------------------------------------------------------------------------------------
+/**
+ * Function called by the Data Hub to deliver to us an update to our lowPower setting.
+ */
+//--------------------------------------------------------------------------------------------------
+static void HandleLowPowerPush
+(
+    double timestamp,
+    bool value,     ///< true = low power mode (ULP), false = not as low but still low power (LP).
+    void* contextPtr
+)
+{
+    mangOH_bme680_SamplingRate_t newRate;
+    if (value)
+    {
+        newRate = MANGOH_BME680_SAMPLING_RATE_ULP;
+    }
+    else
+    {
+        newRate = MANGOH_BME680_SAMPLING_RATE_LP;
+    }
+
+    if (newRate != SamplingRate)
+    {
+        SamplingRate = newRate;
+        UpdateSensor();
+    }
 }
 
 
@@ -174,19 +227,15 @@ COMPONENT_INIT
     io_SetJsonExample(RES_PATH_VALUE, VALUE_EXAMPLE);
 
     LE_ASSERT_OK(io_CreateOutput(RES_PATH_AMBIENT_TEMP, IO_DATA_TYPE_NUMERIC, "degC"));
-    io_AddNumericPushHandler(RES_PATH_AMBIENT_TEMP, AmbientTempPushHandler, NULL);
+    io_AddNumericPushHandler(RES_PATH_AMBIENT_TEMP, HandleAmbientTempPush, NULL);
 
     mangOH_bme680_AddSensorReadingHandler(SensorReadingHandler, NULL);
 
-#ifdef BME680_DHUB_CONFIG
-    le_result_t res = io_CreateOutput(RES_PATH_CONFIG, IO_DATA_TYPE_JSON, "");
-    LE_FATAL_IF(res != LE_OK && res != LE_DUPLICATE,
-                "Couldn't create config data hub output - %s", LE_RESULT_TXT(res));
-    io_MarkOptional(RES_PATH_CONFIG);
-    io_SetJsonExample(RES_PATH_CONFIG, CONFIG_EXAMPLE);
-    io_AddJsonPushHandler(RES_PATH_CONFIG, BmeConfigPushHandler, NULL);
-#else
-    LE_ASSERT_OK(mangOH_bme680_Configure(MANGOH_BME680_SAMPLING_RATE_LP, true, true, true, true,
-                                         true, true));
-#endif // BME680_DHUB_CONFIG
+    LE_ASSERT_OK(io_CreateOutput(RES_PATH_ENABLE, IO_DATA_TYPE_BOOLEAN, ""));
+    io_AddBooleanPushHandler(RES_PATH_ENABLE, HandleEnablePush, NULL);
+
+    LE_ASSERT_OK(io_CreateOutput(RES_PATH_LOW_POWER, IO_DATA_TYPE_BOOLEAN, ""));
+    io_AddBooleanPushHandler(RES_PATH_LOW_POWER, HandleLowPowerPush, NULL);
+    io_MarkOptional(RES_PATH_LOW_POWER);
+    io_SetBooleanDefault(RES_PATH_LOW_POWER, false);
 }
